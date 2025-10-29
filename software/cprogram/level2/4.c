ce0c66f70cb38a5bc28db26f4e987f00a9090350
haransivashankarmz25-dot <haransivashankar.mz25@bitsathy.ac.in>
2025-10-29 11:53:16 +0530

    Add files via upload

---
+++ software/cprogram/level2/4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int num1,num2,num;
+   scanf("%d %d",&num1,&num2);
+   num=num1;
+   for(int i=0;i<num2;i++)
+   {
+    printf("%d+",num);
+     num=num*10+num1;
+   }  
+}
